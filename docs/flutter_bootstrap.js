989d226659f20d02bdaffc337dcea017395dcb04
designcelestialhilllandtech <design.celestialhilllandtech@gmail.com>
2025-10-07 10:50:00 +0530

    Update site

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "372256251"
+    serviceWorkerVersion: "583025126"
   }
 });
